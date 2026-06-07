73a794a8c578f0e5a10a1c7223559216824ffc06
Joshua Peisach <itzswirlz2020@outlook.com>
2026-06-07 18:01:11 +0000

    Rename integration_blueprint to ha_mediawiki
    Signed-off-by: Joshua Peisach <itzswirlz2020@outlook.com>

--- custom_components/ha_mediawiki/__init__.py
+++ custom_components/ha_mediawiki/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+Custom integration to integrate ha_mediawiki with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/ludeeus/ha_mediawiki
 """
 
 from __future__ import annotations
--- custom_components/ha_mediawiki/coordinator.py
+++ custom_components/ha_mediawiki/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for ha_mediawiki."""
 
 from __future__ import annotations
 
