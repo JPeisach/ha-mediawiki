e05160e27e6cfb8f29c69a22f09f3024446a8899
Joshua Peisach <itzswirlz2020@outlook.com>
2026-07-08 19:29:37 +0000

    BlueprintFlowHandler -> MediaWikiFlowHandler
    Signed-off-by: Joshua Peisach <itzswirlz2020@outlook.com>

--- custom_components/ha_mediawiki/config_flow.py
+++ custom_components/ha_mediawiki/config_flow.py
@@ -17,7 +17,7 @@ from .api import (
 from .const import DOMAIN, LOGGER
 
 
-class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class MediaWikiFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for MediaWiki Integration."""
 
     VERSION = 1
